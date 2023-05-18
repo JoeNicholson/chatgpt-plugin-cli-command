fd4db7d3d94965210d50b0170e81bb56c47dbfa3
Joe Nicholson <joe@rufilla.com>
2023-05-18 13:33:21 +0100

    Return 400 if no command sent.  Add more docstrings.

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ cwd = settings.get("working_directory", ".")
 
 app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com")
 
+
 @app.post("/command")
 async def command():
     """
@@ -24,7 +25,9 @@ async def command():
     and returns the output. If the command fails, the function returns the error message.
     """
     data = await request.get_json()
-    command = data.get("command", "")
+    command = data.get("command")
+    if not command:
+        return quart.Response(response="No command provided", status=400)
     logging.info(f"Received command: {command}")
 
     try:
@@ -41,22 +44,38 @@ async def command():
  
 @app.get("/logo.png")
 async def plugin_logo():
+    """
+    Serve the plugin logo.
+
+    This function returns the logo.png file to the client.
+    """
     filename = "logo.png"
     return await quart.send_file(filename, mimetype="image/png")
 
+
 @app.get("/.well-known/ai-plugin.json")
 async def plugin_manifest():
-    host = request.headers["Host"]
+    """
+    Serve the plugin manifest.
+
+    This function reads the ai-plugin.json file and returns it to the client.
+    """
     with open("./.well-known/ai-plugin.json") as f:
         text = f.read()
         return quart.Response(text, mimetype="text/json")
 
+
 @app.get("/openapi.yaml")
 async def openapi_spec():
-    host = request.headers["Host"]
+    """
+    Serve the OpenAPI specification.
+
+    This function reads the openapi.yaml file and returns it to the client.
+    """
     with open("openapi.yaml") as f:
         text = f.read()
         return quart.Response(text, mimetype="text/yaml")
+
 
 def main():
     """
